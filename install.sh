9e5516c52711241be5f0f1ce43a3b84e241e2982
Jan Vlk <jan.vlk@mensa.cz>
2024-12-04 19:34:41 +0100

    fix - uncomment commented lines

--- install.sh
+++ install.sh
@@ -13,18 +13,18 @@ pip install pyaml tqdm tensorboard nuscenes-devkit pandas transforms3d
 pip install -e WaffleIron/
 
 ##### Download models and install ScaLR
-# cd ScaLR
+cd ScaLR
 
-# wget https://github.com/valeoai/ScaLR/releases/download/v0.1.0/info_datasets.tar.gz
-# tar -xvzf info_datasets.tar.gz
-# rm info_datasets.tar.gz
+wget https://github.com/valeoai/ScaLR/releases/download/v0.1.0/info_datasets.tar.gz
+tar -xvzf info_datasets.tar.gz
+rm info_datasets.tar.gz
 
-# wget https://github.com/valeoai/ScaLR/releases/download/v0.1.0/WI_768-DINOv2_ViT_L_14-NS_KI_PD-linear_probing-nuscenes.tar.gz
-# tar -xvzf WI_768-DINOv2_ViT_L_14-NS_KI_PD-linear_probing-nuscenes.tar.gz
-# rm WI_768-DINOv2_ViT_L_14-NS_KI_PD-linear_probing-nuscenes.tar.gz
+wget https://github.com/valeoai/ScaLR/releases/download/v0.1.0/WI_768-DINOv2_ViT_L_14-NS_KI_PD-linear_probing-nuscenes.tar.gz
+tar -xvzf WI_768-DINOv2_ViT_L_14-NS_KI_PD-linear_probing-nuscenes.tar.gz
+rm WI_768-DINOv2_ViT_L_14-NS_KI_PD-linear_probing-nuscenes.tar.gz
 
-# wget https://github.com/valeoai/ScaLR/releases/download/v0.1.0/WI_768-DINOv2_ViT_L_14-NS_KI_PD-pretrained.tar.gz
-# tar -xvzf WI_768-DINOv2_ViT_L_14-NS_KI_PD-pretrained.tar.gz
-# rm WI_768-DINOv2_ViT_L_14-NS_KI_PD-pretrained.tar.gz
+wget https://github.com/valeoai/ScaLR/releases/download/v0.1.0/WI_768-DINOv2_ViT_L_14-NS_KI_PD-pretrained.tar.gz
+tar -xvzf WI_768-DINOv2_ViT_L_14-NS_KI_PD-pretrained.tar.gz
+rm WI_768-DINOv2_ViT_L_14-NS_KI_PD-pretrained.tar.gz
 
-# cd ../
+cd ../
