8279862f6fa4e5f041449cfac5b84906a619d34e
Jan Vlk <jan.vlk@mensa.cz>
2024-12-05 17:35:54 +0100

    add ego vehicle points removal

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@
 import sklearn
 
 from pathlib import Path
-from typing import Union, Optional
+from typing import Union, Optional, Tuple
 
 import torch
 import numpy as np
@@ -114,26 +114,7 @@ def get_points_in_box(
     assert pcd.shape[1] == 3, "Input data must have shape (N, 3)"
     assert box.shape == (7,), "Box must have shape (7,)"
 
-    # Get rotation matrix and translation vector
-    rot_mat = torch.tensor(
-        [
-            [torch.cos(box[6]), -torch.sin(box[6])],
-            [torch.sin(box[6]), torch.cos(box[6])],
-        ],
-        device=device,
-    )
-    translation = torch.tensor([box[0], box[1]], device=device)
-
-    # Transform pcd to box frame
-    pcd_r = (pcd[:, :2] - translation) @ rot_mat
-
-    # Get mask of points inside the box
-    mask = (
-        (-box[3] / 2 <= pcd_r[:, 0])
-        & (pcd_r[:, 0] <= box[3] / 2)
-        & (-box[4] / 2 <= pcd_r[:, 1])
-        & (pcd_r[:, 1] <= box[4] / 2)
-    )
+    mask = get_box_mask(pcd, box)
 
     return pcd[mask]
 
@@ -165,3 +146,56 @@ def rot_matrix_from_Euler(alpha: float, beta: float, gamma: float) -> np.ndarray
         ]
     )
     return ret
+
+
+def get_box_mask(pcd: Union[torch.Tensor, np.ndarray], box: Union[torch.Tensor, np.ndarray]) -> torch.Tensor:
+    device = pcd.device if isinstance(pcd, torch.Tensor) else "cpu"
+    if isinstance(pcd, np.ndarray):
+        pcd = torch.tensor(pcd, device=device)
+    if isinstance(box, np.ndarray):
+        box = torch.tensor(box, device=device)
+    assert pcd.ndim == 2, "Input data must have shape (N, 3)"
+    assert pcd.shape[1] == 3, "Input data must have shape (N, 3)"
+    assert box.shape == (7,), "Box must have shape (7,)"
+
+    # Get rotation matrix and translation vector
+    rot_mat = torch.tensor(
+        [
+            [torch.cos(box[6]), -torch.sin(box[6])],
+            [torch.sin(box[6]), torch.cos(box[6])],
+        ],
+        device=device,
+    )
+    translation = torch.tensor([box[0], box[1]], device=device)
+
+    # Transform pcd to box frame
+    pcd_r = (pcd[:, :2] - translation) @ rot_mat
+
+    # Get mask of points inside the box
+    mask = (
+        (-box[3] / 2 <= pcd_r[:, 0])
+        & (pcd_r[:, 0] <= box[3] / 2)
+        & (-box[4] / 2 <= pcd_r[:, 1])
+        & (pcd_r[:, 1] <= box[4] / 2)
+    )
+
+    return mask
+
+
+def remove_ego_vehicle(pcd: Union[torch.Tensor, np.ndarray], dataset: str) -> Tuple[torch.Tensor, torch.Tensor]:
+    device = pcd.device if isinstance(pcd, torch.Tensor) else "cpu"
+    if isinstance(pcd, np.ndarray):
+        pcd = torch.tensor(pcd, device=device)
+    assert pcd.ndim == 2, "Input data must have shape (N, M)"
+    
+    if dataset == "av2":
+        ego_box = torch.tensor([0, 0, 0, 4.841, 1.834, 1.445, 0])
+    elif dataset == "nuscenes":
+        ego_box = torch.tensor([0, 0, 0, 4.084, 1.730, 1.562, 0])
+    else:
+        raise ValueError(f"Dataset {dataset} not supported")
+
+    mask = get_box_mask(pcd[:,:3], ego_box)
+
+    return pcd[~mask], ~mask
+
