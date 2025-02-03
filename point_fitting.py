881a53cf7a113322bb16c23fd796c4c7d285ea05
Jan Vlk <jan.vlk@mensa.cz>
2025-02-03 13:54:45 +0100

    refactor code, add comments

--- point_fitting.py
+++ point_fitting.py
@@ -7,6 +7,10 @@ import open3d as o3d
 from utils import rot_matrix_from_Euler
 
 
+FITNESS_THRESHOLD = 0.3
+DISTANCE_THRESHOLD = 0.4
+
+
 def icp_transform(
     source: Union[torch.Tensor, np.ndarray],
     sample: Union[torch.Tensor, np.ndarray],
@@ -36,24 +40,26 @@ def icp_transform(
     N = 20
     translate = np.linspace(-2, 2, N)
 
+    # Initialize the point clouds and move the point clouds to their centroid
     source_pcd = o3d.geometry.PointCloud()
     source_pcd.points = o3d.utility.Vector3dVector(source - np.mean(source, axis=0))
     target_pcd = o3d.geometry.PointCloud()
     target_pcd.points = o3d.utility.Vector3dVector(sample - np.mean(sample, axis=0))
     init_transform = np.eye(4)
 
-    source_pcd.paint_uniform_color([1, 0, 0])
-    target_pcd.paint_uniform_color([0, 1, 0])
+    # Color and save the point clouds
     if save_name is not None:
+        source_pcd.paint_uniform_color([1, 0, 0])
+        target_pcd.paint_uniform_color([0, 1, 0])
         merged_pcd = source_pcd + target_pcd
         o3d.io.write_point_cloud(save_name, merged_pcd)
-    # o3d.visualization.draw_geometries([source_pcd, target_pcd])
 
+    # Run the ICP transformation
     best_icp_result = None
     for i in range(N):
-        for j in range(N):
+        for j in range(N // 4, N - (N // 4)):
             for k in range(N):
-                init_transform[:3, 3] = np.array([translate[i], translate[j], 0])
+                init_transform[:3, 3] = np.array([translate[i], 0, translate[j]])
                 init_transform[:3, :3] = rot_matrix_from_Euler(0, 0, k * 2 * np.pi / N)
                 icp_result = o3d.pipelines.registration.registration_icp(
                     source=source_pcd,
@@ -65,8 +71,6 @@ def icp_transform(
                 if (
                     best_icp_result is None
                     or icp_result.fitness > best_icp_result.fitness
-                    # or (icp_result.inlier_rmse < best_icp_result.inlier_rmse and
-                    #     abs(icp_result.fitness - best_icp_result.fitness) < 0.225)
                 ):
                     best_icp_result = icp_result
 
@@ -78,6 +82,7 @@ def good_match(
     sample: Union[torch.Tensor, np.ndarray],
     icp_result,
     save_name: Optional[str] = None,
+    visualize: bool = False,
 ) -> bool:
     """
     Get the good match between two point clouds
@@ -89,6 +94,10 @@ def good_match(
         point cloud to be transformed
     icp_result: o3d.pipelines.registration.RegistrationResult
         the result of the ICP transformation
+    save_name: str
+        the name of the file to save the point clouds
+    visualize: bool
+        visualize the point clouds
     """
     if isinstance(source, torch.Tensor):
         source = source.cpu().numpy()
@@ -99,16 +108,16 @@ def good_match(
     assert sample.ndim == 2, "Target data must have shape (M, 3)"
     assert sample.shape[1] == 3, "Target data must have shape (M, 3)"
 
-    fitness = icp_result.fitness
-    rmse = icp_result.inlier_rmse
-
+    # Initialize the point clouds and move the point clouds to their centroid
     source_pcd = o3d.geometry.PointCloud()
     source_pcd.points = o3d.utility.Vector3dVector(source - np.mean(source, axis=0))
     target_pcd = o3d.geometry.PointCloud()
     target_pcd.points = o3d.utility.Vector3dVector(sample - np.mean(sample, axis=0))
     source_pcd.transform(icp_result.transformation)
-    # print(icp_result.transformation)
 
+    # Compute the fitness, RMSE, and symetric distance
+    fitness = icp_result.fitness
+    rmse = icp_result.inlier_rmse
     source_to_target = np.mean(
         np.asarray(source_pcd.compute_point_cloud_distance(target_pcd))
     )
@@ -121,12 +130,16 @@ def good_match(
     print(f"RMSE: {rmse}")
     print(f"Symetric distance: {symetric_distance}")
 
-    source_pcd.paint_uniform_color([1, 0, 0])
-    target_pcd.paint_uniform_color([0, 1, 0])
-    if save_name is not None:
+    # Color and save the point clouds
+    if save_name is not None or visualize:
+        source_pcd.paint_uniform_color([1, 0, 0])
+        target_pcd.paint_uniform_color([0, 1, 0])
         merged_pcd = source_pcd + target_pcd
-        o3d.io.write_point_cloud(save_name, merged_pcd)
-    # o3d.visualization.draw_geometries([source_pcd, target_pcd])
+        if visualize:
+            o3d.visualization.draw_geometries([merged_pcd])
+        else:
+            o3d.io.write_point_cloud(save_name, merged_pcd)
 
+    # Determine if the match is good
     # TODO: Set hyperparameters
-    return fitness > 0.3 and symetric_distance < 0.4
+    return fitness > FITNESS_THRESHOLD and symetric_distance < DISTANCE_THRESHOLD
