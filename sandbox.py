4f1b7ee11f272552f58cd854fe7ae467dc917209
Jan Vlk <jan.vlk@mensa.cz>
2025-02-04 17:45:12 +0100

    new tests in sandbox

--- sandbox.py
+++ sandbox.py
@@ -3,46 +3,23 @@
 
 import sklearn
 
+import time
 import argparse
 
 import torch
 import numpy as np
-import pandas as pd
+import open3d as o3d
+
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-# from ground_removal import GroundRemoval
-from transform_box import vis_box_bev
-from utils import visualize_pcd, get_clusters, quaternion_to_yaw, load_boxes
-from utils import get_points_in_box, load_nuScenes, load_pcd
+from utils import visualize_pcd, get_clusters
 from point_fitting import icp_transform, good_match
-
-
-INTEREST_CLASSES_AV2 = ["BOX_TRUCK", "BUS", "LARGE_VEHICLE", "REGULAR_VEHICLE"]
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--data_root", type=str, help="Path to the root data directory")
-    parser.add_argument("-fp", "--file_pcd", type=str, help="Path to PCD file")
-    parser.add_argument("-fb", "--file_box", type=str, help="Path to PCD file")
-    parser.add_argument(
-        "--dataset",
-        type=str,
-        default="av2",
-        required=True,
-        help="Name of the dataset, default: av2",
-    )
-    parser.add_argument(
-        "--save_dir",
-        type=str,
-        help="Path to save the results, if not specified, the results will be saved in <data_dir>_xyz",
-    )
-    parser.add_argument(
-        "--visualize", action="store_true", help="Visualize the results"
-    )
-    parser.add_argument("--verbose", action="store_true", help="Print verbose messages")
     parser.add_argument("--eps", type=float, default=0.7, help="DBSCAN epsilon")
     parser.add_argument(
         "--min_points", type=int, default=10, help="DBSCAN minimum points"
@@ -53,93 +30,72 @@ def parse_args() -> argparse.Namespace:
 
 if __name__ == "__main__":
     args = parse_args()
-    if args.file_pcd is None and args.data_root is None:
-        raise ValueError("Either --file or --data_root must be provided")
-    elif args.file_pcd is not None and args.data_root is not None:
-        raise ValueError("Only one of --file or --data_root must be provided")
 
-    # ground_removal = GroundRemoval(args, standalone=True)
-    if args.dataset == "nuscenes":
-        if args.data_root is None:
-            raise ValueError("For nuScenes --data_root must be provided")
-    else:
-        if args.file_pcd is not None:
-            # pcd, _ = ground_removal.run_individual_file(args.file_pcd)
-            pcd = load_pcd(args.file_pcd, args.dataset)
-            labels = get_clusters(pcd, eps=args.eps, min_points=args.min_points)
-            if args.visualize:
-                visualize_pcd(pcd, labels)
+    # Load reference vehicles
+    ref_veh = o3d.io.read_point_cloud("exports/cloud_test.ply")
+    ref_veh_points = np.asarray(ref_veh.points)
+    ref_veh_colors = np.asarray(ref_veh.colors)
+    ref_veh1 = ref_veh_points[np.all(ref_veh_colors == [1, 0, 0], axis=1)]
+    ref_veh2 = ref_veh_points[np.all(ref_veh_colors == [0, 1, 0], axis=1)]
 
-            if args.file_box is not None:
-                ego_box = torch.tensor([0, 0, 0, 4.841, 1.834, 1.445, 0])
-                boxes = load_boxes(args.file_box, args.dataset)
-                timestamp = boxes.iloc[0]["timestamp_ns"]
+    # Load and preprocess the point cloud
+    data = np.load("exports/scene-0158.npy", allow_pickle=True).T
+    labels = get_clusters(data, args.eps, args.min_points)
+    cluster_num = len(np.unique(labels))
+    # print(f"Number of clusters: {cluster_num}")
 
-                labels = boxes["category"].unique()
-                labels = np.append(labels, "EGO_VEHICLE")
-                colors = plt.get_cmap("tab20")(np.linspace(0, 1, len(labels)))
-                legend_handles = [
-                    mpatches.Patch(color=colors[i], label=labels[i])
-                    for i in range(len(labels))
-                ]
+    # Do ICP
+    cluster1 = data[labels == 0]
+    cluster2 = data[labels == 1]
 
-                for i in range(boxes.shape[0] - 1):
-                    box = boxes.iloc[i]
-                    box2 = boxes.iloc[i + 1]
-                    if (
-                        box["category"] not in INTEREST_CLASSES_AV2
-                        or box2["category"] not in INTEREST_CLASSES_AV2
-                    ):
-                        continue
-                    if timestamp != box["timestamp_ns"]:
-                        timestamp = box["timestamp_ns"]
-                        # vis_box_bev(ego_box, colors[-1])
-                        # plt.title(f"Timestamp: {timestamp}")
-                        # plt.legend(handles=legend_handles)
-                        # plt.scatter(pcd[:, 0], pcd[:, 1], s=1, c="black")
-                        # plt.show()
-                        exit()
-                    box_r = torch.tensor(
-                        [
-                            box["tx_m"],
-                            box["ty_m"],
-                            box["tz_m"],
-                            box["length_m"],
-                            box["width_m"],
-                            box["height_m"],
-                            quaternion_to_yaw(
-                                torch.tensor(
-                                    [box["qw"], box["qx"], box["qy"], box["qz"]]
-                                )
-                            ),
-                        ]
-                    )
-                    box_r_2 = torch.tensor(
-                        [
-                            box2["tx_m"],
-                            box2["ty_m"],
-                            box2["tz_m"],
-                            box2["length_m"],
-                            box2["width_m"],
-                            box2["height_m"],
-                            quaternion_to_yaw(
-                                torch.tensor(
-                                    [box2["qw"], box2["qx"], box2["qy"], box2["qz"]]
-                                )
-                            ),
-                        ]
-                    )
-                    object_pcd = get_points_in_box(pcd, box_r)
-                    object_pcd_2 = get_points_in_box(pcd, box_r_2)
-                    icp_res = icp_transform(object_pcd, object_pcd_2)
-                    good_match(object_pcd, object_pcd_2, icp_res)
-                    # if box["category"] in INTEREST_CLASSES_AV2:
-                    #     object_pcd = get_points_in_box(pcd, box_r)
-                    #     print(f"Object type: {box['category']}")
-                    #     if object_pcd.shape[0] != 0:
-                    #         visualize_pcd(object_pcd, None)
-                    # label = box["category"]
-                    # color = colors[np.where(labels == label)[0][0]]
-                    # vis_box_bev(box_r, color)
+    cluster_cent1 = (np.min(cluster1, axis=0) + np.max(cluster1, axis=0)) / 2
+    cluster_cent2 = (np.min(cluster2, axis=0) + np.max(cluster2, axis=0)) / 2
 
-                # plt.show()
+    cluster_cent1 = np.mean(cluster1, axis=0)
+    cluster_cent2 = np.mean(cluster2, axis=0)
+
+    # Plot the 3D points and bounding box
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    points = cluster1 - cluster_cent1
+
+    # Plot points
+    ax.scatter(
+        points[:, 0],
+        points[:, 1],
+        points[:, 2],
+        c="red",
+        marker="o",
+        label="Points",
+        s=5,
+    )
+
+    points = cluster2 - cluster_cent2
+
+    # Plot points
+    ax.scatter(
+        points[:, 0],
+        points[:, 1],
+        points[:, 2],
+        c="blue",
+        marker="o",
+        label="Points",
+        s=5,
+    )
+
+    # Labels and legend
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+    ax.set_title("3D Points and Bounding Box")
+    ax.legend()
+
+    # plt.show()
+    # exit()
+
+    start = time.time()
+    res = icp_transform(cluster1, cluster2)
+    print(f"Time taken: {time.time() - start:.2f} s")
+    good = good_match(
+        cluster1, cluster2, res, save_name="exp/cluster_test.ply", visualize=True
+    )
