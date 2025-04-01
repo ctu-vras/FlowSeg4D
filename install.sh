0a07e72496b118d42f32db87c7f1ffaeb3c05bb4
Jan Vlk <jan.vlk@mensa.cz>
2025-04-01 17:24:31 +0200

    update install script

--- install.sh
+++ install.sh
@@ -6,11 +6,15 @@ ml PyTorch3D/0.7.1-foss-2022a-CUDA-11.7.0
 python3 -m venv unsup3D
 source unsup3D/bin/activate
 
+##### Clone repositories
+git clone https://github.com/valeoai/Alpine.git
+
 ##### Install dependencies
 pip install torch_scatter
 pip install open3d typing_extensions requests pyarrow
 pip install pyaml tqdm tensorboard nuscenes-devkit pandas transforms3d
 pip install -e WaffleIron/
+pip install -e Alpine/
 
 ##### Download models and install ScaLR
 cd ScaLR
